d3f2c5a8bc8189747402e7d3727005218bca6398
Kapil <kapilraut026@gmail.com>
2025-10-05 19:21:47 +0545

    hi

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return <h1>About Page</h1>;
+}
+
+// to add new pages in our app
+// folder in app called page.tsx
+// this is a special file name
+// nextjs will automatically treat it as a route
