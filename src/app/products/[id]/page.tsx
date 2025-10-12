28ff716f6503e131cebadc0626012e7fd1cef194
Kapil <kapilraut026@gmail.com>
2025-10-12 09:37:40 +0545

    hiii

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>id</div>;
+  return <div>It is an elegant</div>;
 };
 
 export default page;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,18 @@
 import Prod from "../../models.json";
+import Link from "next/link";
 
 export default function Products() {
-  return <h1>Products Page</h1>;
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <h1>Products Page</h1>;
+      <ul>
+        {Prod.map((item) => (
+          <li key={item.id}>
+            <Link href={`/products/${item.id}`}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
