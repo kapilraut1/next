87fee2a2e6d8cf4e6219cdd6d1c8a98d80f9b642
Kapil <kapilraut026@gmail.com>
2025-10-16 22:29:31 +0545

    lo

---
+++ src/app/_components/Loading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const LoadingSk = () => {
+  console.log("Loading component rendered");
+  return <div>Loading!!</div>;
+};
+
+export default LoadingSk;
---
+++ src/app/loading.tsx
@@ -0,0 +1,4 @@
+import LoadingSk from "./_components/Loading";
+export default function Loading() {
+  return <LoadingSk />;
+}
